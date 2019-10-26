d4a22f10815d3319d9730ff2ca054a9db1675516
mfekry10 <42072245+mfekry10@users.noreply.github.com>
2019-10-26 22:56:56 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Calculate slider height
+
+	// var winH = $(window).height();
+
+	// var barH  = $('.bar').innerHeight();
+
+	// var containerH =$('.container').innerHeight();
+
+	// $('#carouselExampleControls, #carouselExampleControls .carousel-item').height(winH - (barH + containerH));
